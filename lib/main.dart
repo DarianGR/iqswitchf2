d591639848ce25319900c80c2a2f99023bd04de3
Cesar Acosta <c3sar.adan@gmail.com>
2023-04-13 19:58:08 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Pages/pages.dart';
+import 'services/services.dart';
+
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+      ],
+      child: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IQ-Switch App',
+      initialRoute: 'checking',
+      routes: {
+        'login': (_) => LoginScreen(),
+        'register': (_) => RegisterScreen(),
+        'home': (_) => HomeScreen(),
+        'checking': (_) => CheckAuthScreen()
+      },
+      scaffoldMessengerKey: NotificationsService.messengerKey,
+      theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: Colors.grey[300],
+          appBarTheme: const AppBarTheme(elevation: 0, color: Colors.indigo),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Colors.indigo, elevation: 0)),
+    );
+  }
+}
---
+++ lib/services/auth_services.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService extends ChangeNotifier {
+  final String _baseUrl = '192.168.100.6:7150';
+  //final String _firebaseToken = 'AIzaSyBcytoCbDUARrX8eHpcR-Bdrdq0yUmSjf8';
+
+  final storage = new FlutterSecureStorage();
+
+  // Si retornamos algo, es un error, si no, todo bien!
+  Future<String?> createUser(String email, String password) async {
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password,
+      //'returnSecureToken': true
+    };
+
+    final url = Uri.https(_baseUrl, '/api/Cuentas/registrar');
+
+    final resp = await http.post(url, body: json.encode(authData));
+    final Map<String, dynamic> decodedResp = json.decode(resp.body);
+
+    if (decodedResp.containsKey('Token')) {
+      // Token hay que guardarlo en un lugar seguro
+      await storage.write(key: 'token', value: decodedResp['Token']);
+      // decodedResp['idToken'];
+      return null;
+    } else {
+      return decodedResp['error']['message'];
+    }
+  }
+
+  Future<String?> login(String email, String password) async {
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password
+    };
+    final url = Uri.https(_baseUrl, '/api/Cuentas/login');
+
+    final url2 = Uri.https(_baseUrl, '/Prueba/on');
+
+    final resp2 = await http.get(url2, headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Accept': 'application/json'
+    });
+
+    /*final resp = await http.post(url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(authData));*/
+
+    final resp = await http.post(url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json'
+        },
+        body: json.encode(authData));
+
+    final Map<String, dynamic> decodedResp = json.decode(resp.body);
+
+    if (decodedResp.containsKey('Token')) {
+      // Token hay que guardarlo en un lugar seguro
+      // decodedResp['idToken'];
+      await storage.write(key: 'token', value: decodedResp['Token']);
+      return null;
+    } else {
+      return decodedResp['error']['message'];
+    }
+  }
+
+  Future logout() async {
+    await storage.delete(key: 'token');
+    return;
+  }
+
+  Future<String> readToken() async {
+    return await storage.read(key: 'token') ?? '';
+  }
+}
